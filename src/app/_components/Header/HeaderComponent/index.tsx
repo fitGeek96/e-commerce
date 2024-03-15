407f1200c8670e9a0852ff196be59a07516bf199
fitgeek96 <fitgeek96@gmail.com>
2024-03-16 01:20:38 +0200

    Add & Style the Header

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import React from 'react'
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import classes from './index.module.scss'
+import { HeaderNav } from '../Nav'
+import { noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
+  return (
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <Gutter className={classes.container}>
+        <Link href={'/'}>
+          <Image src="/logo.jpg" alt="logo" width={70} height={70} className={classes.logo} />
+        </Link>
+        <HeaderNav header={header} />
+      </Gutter>
+    </nav>
+  )
+}
+
+export default HeaderComponent
