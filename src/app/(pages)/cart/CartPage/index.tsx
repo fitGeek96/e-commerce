308c445e3d8053ea0600ae664ca89b01bf72966c
fitgeek96 <fitgeek96@gmail.com>
2024-03-18 20:12:18 +0200

    Add All the checkout functionalities

--- src/app/(pages)/cart/CartPage/index.tsx
+++ src/app/(pages)/cart/CartPage/index.tsx
@@ -98,16 +98,16 @@ export const CartPage: React.FC<{
 
               <div className={classes.summary}>
                 <div className={classes.row}>
-                  <h6 className={classes.cartTotal}>Summary</h6>
+                  <h6 className={classes.cartTotal}>Résumé de la commande</h6>
                 </div>
 
                 <div className={classes.row}>
-                  <p className={classes.cartTotal}>Delivery Charge</p>
+                  <p className={classes.cartTotal}>Frais de livraison</p>
                   <p className={classes.cartTotal}>DZD 0 </p>
                 </div>
 
                 <div className={classes.row}>
-                  <p className={classes.cartTotal}>Grand Total</p>
+                  <p className={classes.cartTotal}>Total</p>
                   <p className={classes.cartTotal}>{cartTotal.formatted}</p>
                 </div>
 
--- src/app/(pages)/order-confirmation/OrderConfirmationPage/index.tsx
+++ src/app/(pages)/order-confirmation/OrderConfirmationPage/index.tsx
@@ -39,14 +39,15 @@ export const OrderConfirmationPage: React.FC<{}> = () => {
         </Fragment>
       ) : (
         <Fragment>
-          <h1>Thank you for your order!</h1>
+          <h1>Merci pour votre commande !</h1>
           <p>
-            {`Your order has been confirmed. You will receive an email confirmation shortly. Your order ID is ${orderID}.`}
+            Votre commande a été confirmée. Vous recevrez sous peu un SMS de confirmation. Votre
+            numéro de commande est <strong>{orderID}</strong>.
           </p>
           <div className={classes.actions}>
-            <Button href={`/orders/${orderID}`} label="View order" appearance="primary" />
+            <Button href={`/account/orders/${orderID}`} label="View order" appearance="primary" />
             <Button
-              href={`${process.env.NEXT_PUBLIC_SERVER_URL}/orders`}
+              href={`${process.env.NEXT_PUBLIC_SERVER_URL}/account/orders`}
               label="View all orders"
               appearance="secondary"
             />
