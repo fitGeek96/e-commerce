ce427201079a1c0a4b4b21efaf3cf4bf33e7b5e5
fitgeek96 <fitgeek96@gmail.com>
2024-03-19 02:39:03 +0200

    another .eslint.js modification

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   rules: {
-    "extends": "off",
     "rules": {
       "react/no-unescaped-entities": "off",
       "@next/next/no-page-custom-font": "off"
