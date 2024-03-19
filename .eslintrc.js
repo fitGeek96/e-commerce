cb373e4406d53d6cc67f06b0002fea39a5f60fdc
fitgeek96 <fitgeek96@gmail.com>
2024-03-19 15:12:55 +0200

    modify .eslint file and added rules for prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,15 @@
-module.exports = {
+export default {
   root: true,
-  extends: ['plugin:@next/next/recommended', '@payloadcms'],
-  ignorePatterns: ['**/payload-types.ts'],
+  extends: ["plugin:@next/next/recommended", "@payloadcms"],
+  ignorePatterns: ["**/payload-types.ts"],
   rules: {
-    "rules": {
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-page-custom-font": "off"
-    }
-  }
-}
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
